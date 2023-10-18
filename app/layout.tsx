56c4ce0cf1ca3a461b9ecb2c83ad8ca32cebc515
zrjohnson3 <zrjohnson3@gmail.com>
2023-10-18 16:07:15 -0400

    Added a Markdown Editor and Styled Radix-UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,17 @@
 import '@radix-ui/themes/styles.css';
+import './theme-config.css'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import NavBar from './NavBar'
-import { Theme } from '@radix-ui/themes';
+import { Theme, ThemePanel } from '@radix-ui/themes';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+  variable: '--font-inter',
+});
+
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -19,10 +25,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
-        <Theme>
+      <body className={inter.variable}>
+        <Theme accentColor="purple" radius="large">
           <NavBar />
           <main className='p-5'>{children}</main>
+          {/* <ThemePanel /> */} {/* Used to adjust radix UI styling in browser */}
         </Theme>
       </body>
     </html>

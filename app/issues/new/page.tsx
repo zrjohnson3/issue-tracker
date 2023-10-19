cb7f76dd8a945577570c578252002e2576e16139
zrjohnson3 <zrjohnson3@gmail.com>
2023-10-19 11:03:06 -0400

    Added a Spinner

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -11,17 +11,26 @@ import { zodResolver } from '@hookform/resolvers/zod';
 import { createIssueSchema } from '@/app/validationSchemas';
 import { z } from 'zod';
 import ErrorMessage from '@/app/components/ErrorMessage';
+import Spinner from '@/app/components/Spinner';
 
 
 // Let zod infer type of IssueForm based on schema
 type IssueForm = z.infer<typeof createIssueSchema>;
 
 const NewIssuePage = () => {
-    const router = useRouter(); //appRouter/navigation is one that is used with app route
+
+    // Navigation - appRouter/navigation is one that is used with app route
+    const router = useRouter();
+
+    // React Hook Form and zod schema validation
     const { register, control, handleSubmit, formState: { errors, isValid } } = useForm<IssueForm>({
         resolver: zodResolver(createIssueSchema),
     });
+
+    // State Varaiables
     const [error, setError] = useState('');
+    const [isSubmitting, setIsSubmitting] = useState(false);
+
     console.log(register('title'));
     return (
         <>
@@ -37,10 +46,12 @@ const NewIssuePage = () => {
                     // onSubmit={handleSubmit((data) => { console.log(data) })}>
                     onSubmit={handleSubmit(async (data) => {
                         try {
+                            setIsSubmitting(true);
                             await axios.post('/api/issues', data);
                             router.push('/issues');
                         } catch (error) {
                             //console.log(error);
+                            setIsSubmitting(false);
                             setError('An unexpected error occured');
                         }
 
@@ -55,7 +66,7 @@ const NewIssuePage = () => {
                         render={({ field }) => <SimpleMDE placeholder='Description' {...field} />}
                     />
                     {<ErrorMessage>{errors.description?.message}</ErrorMessage>}
-                    <Button>Submit New Issue</Button>
+                    <Button disabled={isSubmitting}>Submit New Issue {isSubmitting && <Spinner />}</Button>
                 </form>
             </div>
         </>

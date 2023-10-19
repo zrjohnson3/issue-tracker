d3544041ce186ee12710153f32d8a39c73fabd65
zrjohnson3 <zrjohnson3@gmail.com>
2023-10-19 16:06:46 -0400

    Added the edit button

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import { notFound } from 'next/navigation'
 import prisma from '@/prisma/client'
 import IssueStatusBadge from '@/app/components/IssueStatusBadge'
-import { Card, Flex, Heading, Text } from '@radix-ui/themes'
+import { Box, Button, Grid, Card, Flex, Heading, Text } from '@radix-ui/themes'
 import ReactMarkdown from 'react-markdown'
+import { Pencil2Icon } from '@radix-ui/react-icons';
+import Link from 'next/link';
 
 interface Props {
     params: { id: string }
@@ -18,17 +20,25 @@ const IssueDetailPage = async ({ params }: Props) => {
         notFound();
     }
     return (
-        <div>
-            <Heading>{issue.title}</Heading>
-            <Flex className='space-x-3' my={"2"}>
-                <IssueStatusBadge status={issue.status} />
-                <Text>{issue.createdAt.toDateString()}</Text>
-            </Flex>
-            <Card className='prose'>
-                {/* <p>{issue.description}</p> */}
-                <ReactMarkdown>{issue.description}</ReactMarkdown>
-            </Card>
-        </div>
+        <Grid columns={{ initial: "1", md: "2" }} gap={"5"}>
+            <Box>
+                <Heading>{issue.title}</Heading>
+                <Flex className='space-x-3' my={"2"}>
+                    <IssueStatusBadge status={issue.status} />
+                    <Text>{issue.createdAt.toDateString()}</Text>
+                </Flex>
+                <Card className='prose'>
+                    {/* <p>{issue.description}</p> */}
+                    <ReactMarkdown>{issue.description}</ReactMarkdown>
+                </Card>
+            </Box>
+            <Box>
+                <Button>
+                    <Pencil2Icon />
+                    <Link href={`/issues/${issue.id}/edit`}>Edit</Link>
+                </Button>
+            </Box>
+        </Grid>
     )
 }
 

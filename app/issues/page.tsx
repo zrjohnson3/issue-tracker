29df090da8b13e1fd41e385a49c758740dac4f53
zrjohnson3 <zrjohnson3@gmail.com>
2023-10-19 12:04:31 -0400

    Refactored Issue Page and styling

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 import React, { useState, useEffect } from 'react'
-import { Button } from '@radix-ui/themes'
+import { Button, Table } from '@radix-ui/themes'
 import Link from 'next/link'
 import axios from 'axios';
 import { Issue } from '@prisma/client';
@@ -28,9 +28,44 @@ const IssuePage = () => {
 
     return (
         <div>
-            <h3>IssuesPage</h3><br />
+            <h2 className='text-3xl text-center items-center'>Issues Page</h2><br />
 
-            <table className='table-auto box-border border-seperate border border-spacing-3 border-stone-600'>
+
+            <Button>
+                <Link href={"/issues/new"}>New Issue</Link>
+            </Button>
+
+            <br />
+
+            <Table.Root className='py-3'>
+                <Table.Header>
+                    <Table.Row>
+                        <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Title</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Description</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Status</Table.ColumnHeaderCell>
+                        <Table.ColumnHeaderCell className='hidden md:table-cell'>Created</Table.ColumnHeaderCell>
+                    </Table.Row>
+                </Table.Header>
+                <Table.Body>
+                    {issues.map((issue: any) => (
+                        <Table.Row key={issue.id}>
+                            <Table.Cell className='hidden md:table-cell'>{issue.id}</Table.Cell>
+                            <Table.Cell>
+                                <Link href={`/issues/${issue.id}`}>
+                                    <p className='hover:underline'>{issue.title}</p>
+                                </Link>
+                            </Table.Cell>
+
+                            <Table.Cell className='hidden md:table-cell'>{issue.description}</Table.Cell>
+                            <Table.Cell className='hidden md:table-cell'>{issue.status}</Table.Cell>
+                            <Table.Cell className='hidden md:table-cell'>{issue.createdAt}</Table.Cell>
+                        </Table.Row>
+                    ))}
+                </Table.Body>
+            </Table.Root>
+
+            {/* <table className='table-auto box-border border-seperate border border-spacing-3 border-stone-600'>
                 <thead>
                     <tr>
                         <th className="border p-4">ID</th>
@@ -55,9 +90,9 @@ const IssuePage = () => {
                         ))
                     )}
                 </tbody>
-            </table>
-            <br />
-            <Button className='items-center text-center'><Link href={"/issues/new"}>New Issue</Link></Button>
+            </table> */}
+            {/* <br />
+            <Button className='items-center text-center'><Link href={"/issues/new"}>New Issue</Link></Button> */}
 
         </div>
     )
